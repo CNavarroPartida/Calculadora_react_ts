d950f6d46aee7316a8af70a089d71373acc249fd
Carlos Navarro <carlos_a._93@hotmail.com>
2025-07-29 20:23:58 -0600

    actualizando a reducer

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,17 +1,18 @@
-import { useMemo } from "react"
+import { Dispatch, useMemo } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
+import { OrderActions } from "../reducers/order-reducer"
 
 type OrderTotalsProps = {
     order: OrderItem[],
     tip: number,
-    placeOrder: () => void
+    dispatch : Dispatch<OrderActions>
 }
 
-export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps) {
+export default function OrderTotals({order, tip, dispatch} : OrderTotalsProps) {
     const subTotalAmount = useMemo( () => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ), [order])
     const tipAmount = useMemo(() => subTotalAmount * tip, [tip, order])
-    const totalAmount = useMemo(() => subTotalAmount + tip, [tip, order])
+    const totalAmount = useMemo(() => subTotalAmount + tipAmount, [tip, order])
     
     return (
         <>
@@ -34,7 +35,7 @@ export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps)
             <button 
                 className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
                 disabled={totalAmount === 0}
-                onClick={() => placeOrder()}>
+                onClick={() => dispatch({type:"place-holder"})}>
                 Guardar Orden
             </button>
         </>
